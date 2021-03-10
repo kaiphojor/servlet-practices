bd5bd2eb5ccd996feb076b61c3e31d79a7ace510
kaiphojor <jihoopark7666@gmail.com>
2021-03-10 16:25:56 +0900

    emaillist01 finish

--- emaillist01/src/main/java/com/bitacademy/emaillist/dao/EmaillistDao.java
+++ emaillist01/src/main/java/com/bitacademy/emaillist/dao/EmaillistDao.java
@@ -11,17 +11,67 @@ import java.util.List;
 import com.bitacademy.emaillist.vo.EmaillistVo;
 
 public class EmaillistDao {
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		//1. jdbc driver loading
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2. 연결하기 
+			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=utf8&serverTimezone=UTC";
+			conn = DriverManager.getConnection(url,"webdb","webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("error " + e);
+		}
+		return conn;
+	}
+	public boolean insert(EmaillistVo vo) {
+		List<EmaillistVo> list = new ArrayList<EmaillistVo>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Connection conn = null;
+		boolean result = false;
+		try {
+			conn = getConnection();
+			//3. sql 준비 
+			String sql ="insert into emaillist "
+					+ " values(null,?,?,?);";
+			pstmt = conn.prepareStatement(sql);
+			// 4. 바인딩
+			pstmt.setString(1, vo.getFirstName());
+			pstmt.setString(2, vo.getLastName());
+			pstmt.setString(3, vo.getEmail());
+			
+			// 5. sql 문 실행 
+			int count = pstmt.executeUpdate();
+			result = 1 == count ;
+			//6 resultSet 가져오기 
+			
+		} catch (SQLException e) {
+			// 1. 사과 
+			// 2. 로그 
+			System.out.println("error:" + e);
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();				
+				}
+			}catch (Exception e) {
+				// TODO: handle exception
+			}			
+		}
+		return result;
+	}
+	// 전체 이메일 목록 조회
 	public List<EmaillistVo> findAll(){
 		List<EmaillistVo> list = new ArrayList<EmaillistVo>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			//1. jdbc driver loading
-			Class.forName("com.mysql.jdbc.Driver");
-			//2. 연결하기 
-			String url = "jdbc:mysql://localhost:3306/webdb?characterEncoding=utf8&serverTimezone=UTC";
-			conn = DriverManager.getConnection(url,"webdb","webdb");
+			conn = getConnection();
 			//3. sql 준비 
 			String sql ="select no, first_name, last_name, email "
 					+ " from emaillist "
@@ -43,11 +93,6 @@ public class EmaillistDao {
 				vo.setEmail(email);
 				list.add(vo);
 			}
-		} catch (ClassNotFoundException e) {
-			// 1. 사과 
-			// 2. 로그 
-			System.out.println("error:" + e);
-			// 3. 안전하게 종료 
 		} catch (SQLException e) {
 			// 1. 사과 
 			// 2. 로그 
--- emaillist01/src/test/java/com/bitacademy/emaillist/dao/test/EmaillistDaoTest.java
+++ emaillist01/src/test/java/com/bitacademy/emaillist/dao/test/EmaillistDaoTest.java
@@ -7,7 +7,18 @@ import com.bitacademy.emaillist.vo.EmaillistVo;
 
 public class EmaillistDaoTest {
 	public static void main(String[] args) {
+		//insert test 
+		testInsert();
+		
+		//findAll test
 		testFindAll();
+	}
+	public static void testInsert() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("전");
+		vo.setLastName("진");
+		vo.setEmail("forward@gmail.com");
+		new EmaillistDao().insert(vo);
 	}
 	public static void testFindAll() {
 		List<EmaillistVo> list = new EmaillistDao().findAll();
