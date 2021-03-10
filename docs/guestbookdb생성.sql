d8c525d3faeca5e8190283d3ed9b6605114c318b
kaiphojor <jihoopark7666@gmail.com>
2021-03-11 08:18:56 +0900

    add guestbook01-sql-docs

---
+++ docs/guestbookdb생성.sql
@@ -0,0 +1,35 @@
+-- 방명록
+CREATE TABLE `guestbook` (
+	`no`       INT UNSIGNED NOT NULL COMMENT '번호', -- 번호
+	`name`     VARCHAR(50)  NOT NULL COMMENT '이름', -- 이름
+	`password` VARCHAR(20)  NOT NULL COMMENT '비밀번호', -- 비밀번호
+	`contents` TEXT         NOT NULL COMMENT '내용', -- 내용
+	`reg_date` DATETIME     NOT NULL COMMENT '등록일' -- 등록일
+)
+COMMENT '방명록';
+
+-- 방명록
+ALTER TABLE `guestbook`
+	ADD CONSTRAINT `PK_guestbook` -- 방명록 기본키
+		PRIMARY KEY (
+			`no` -- 번호
+		);
+
+ALTER TABLE `guestbook`
+	MODIFY COLUMN `no` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '번호';
+
+
+desc guestbook;
+
+-- insert 
+insert
+	into guestbook
+		values(null,'안대혁','1234','테스트입니다.',now());
+        
+-- select
+select no, name,  date_format(reg_date,'%Y-%m-%d %H:%i:%s'),contents
+	from guestbook
+	order by reg_date desc;
+    
+delete from guestbook 
+	where no = 1 and password = '1234';
