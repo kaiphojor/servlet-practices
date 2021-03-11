5581d967ede031af296f893ed57766698e868f2f
kaiphojor <jihoopark7666@gmail.com>
2021-03-11 11:56:46 +0900

    edit GuestbookServlet-delete

--- guestbook02/src/main/java/com/bitacademy/guestbook/controller/GuestbookServlet.java
+++ guestbook02/src/main/java/com/bitacademy/guestbook/controller/GuestbookServlet.java
@@ -19,43 +19,24 @@ import com.bitacademy.web.mvc.WebUtil;
 public class GuestbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		String action = request.getParameter("a");
-		// 방명록 삭제 기능, 처리 후 메인페이지로 redirect
-		if("delete".equals(action)) {
-			String no = request.getParameter("no");
-			int number = -1 ;
-			if(no != null && no.matches("\\d*")){
-				number = Integer.parseInt(no);
-			}
-			String password = request.getParameter("password");
-			GuestbookVo vo = new GuestbookVo();
-			vo.setNo(number);
-			vo.setPassword(password);
-			// 삭제 결과에 따라서 성공시 index.jsp로 이동, 삭제 실패시 deleteform.jsp로 다시 복귀
-			boolean result = new GuestbookDao().delete(vo);
-			if(result){
-				WebUtil.redirect(request.getContextPath()+"/gb", request, response);
-			}else{
-				response.setContentType("text/html; charset=UTF-8"); 
-				PrintWriter writer = response.getWriter(); 
-				writer.println("<script>alert('비밀번호가 일치하지 않습니다.');history.back();</script>"); 
-				writer.close();
-			}
+
 		// 방명록 삭제 화면
-		}else if("deleteform".equals(action)) {
+		if ("deleteform".equals(action)) {
 			String no = request.getParameter("no");
-			int number = -1 ;
-			if(no != null && no.matches("\\d*")){
+			int number = -1;
+			if (no != null && no.matches("\\d*")) {
 				number = Integer.parseInt(no);
 			}
-			request.setAttribute("no",number);
-			WebUtil.forward("/WEB-INF/views/deleteform.jsp",request,response);
-		}else {
+			request.setAttribute("no", number);
+			WebUtil.forward("/WEB-INF/views/deleteform.jsp", request, response);
+		} else {
 			List<GuestbookVo> list = new GuestbookDao().selectAll();
 			request.setAttribute("list", list);
-			WebUtil.forward("/WEB-INF/views/index.jsp",request,response);
+			WebUtil.forward("/WEB-INF/views/index.jsp", request, response);
 		}
 	}
 
@@ -76,7 +57,28 @@ public class GuestbookServlet extends HttpServlet {
 
 			new GuestbookDao().insert(vo);			
 			WebUtil.redirect(request.getContextPath()+"/gb", request, response);
-		}else {
+		// 방명록 삭제 기능, 처리 후 메인페이지로 redirect
+		}else if("delete".equals(action)) {
+			String no = request.getParameter("no");
+			int number = -1 ;
+			if(no != null && no.matches("\\d*")){
+				number = Integer.parseInt(no);
+			}
+			String password = request.getParameter("password");
+			GuestbookVo vo = new GuestbookVo();
+			vo.setNo(number);
+			vo.setPassword(password);
+			// 삭제 결과에 따라서 성공시 index.jsp로 이동, 삭제 실패시 deleteform.jsp로 다시 복귀
+			boolean result = new GuestbookDao().delete(vo);
+			if(result){
+				WebUtil.redirect(request.getContextPath()+"/gb", request, response);
+			}else{
+				response.setContentType("text/html; charset=UTF-8"); 
+				PrintWriter writer = response.getWriter(); 
+				writer.println("<script>alert('비밀번호가 일치하지 않습니다.');history.back();</script>"); 
+				writer.close();
+			}
+		}else{
 			List<GuestbookVo> list = new GuestbookDao().selectAll();
 			request.setAttribute("list", list);
 			WebUtil.forward("/WEB-INF/views/index.jsp",request,response);
